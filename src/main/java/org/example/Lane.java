f1edef4ebb4267cfaa856d3c4bd02467f6fd5aeb
pablo-chacon <Snemilio@protonmail.com>
2022-10-23 11:44:11 +0200

    Small application to get right vehicle type within weight limitations assigned to appropriate lane. HashMap keys lane name, value vehicle data.

---
+++ src/main/java/org/example/Lane.java
@@ -0,0 +1,10 @@
+/*package org.example;
+
+import java.util.HashMap;
+
+public class Lane extends Lanes {
+
+    public Lane(String vehicle, int weight) {
+        super(vehicle, weight);
+    }
+}*/
